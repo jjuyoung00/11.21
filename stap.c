305c926496f5ccb86efec003d680dab6807291d3
jjuyoung00 <55523928+jjuyoung00@users.noreply.github.com>
2019-11-21 15:25:09 +0900

    Add files via upload

---
+++ stap.c
@@ -0,0 +1,17 @@
+Yes, I did it!!!
+응, 내가 했어!!!
+I will do it
+제가 하겠습니다
+I can do it
+나는 할수있다
+I'll try to do it
+내가 해볼게
+How do i do it?
+제가 그걸 어떻게 합니까?
+I wan't do it
+나는 그것을 하지 않을 것이다
+I can't do it
+나는 그것을 할 수 없다
+I wont to do it
+나는 그것을 하지 않을 것이다
+
